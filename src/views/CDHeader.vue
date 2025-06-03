ed29a84d9464a6450a37dc32e2c9d6121bb1cfc1
Stefan Fathi <stefan@fathi.dev>
2025-06-03 10:16:16 +0200

    Initial commit

---
+++ src/views/CDHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <IonHeader>
+    <IonToolbar>
+      <IonTitle>Component Dojo</IonTitle>
+    </IonToolbar>
+  </IonHeader>
+</template>
+
+<script setup>
+import { IonHeader, IonToolbar, IonTitle } from "@ionic/vue";
+</script>
---
+++ src/views/UserPage/UserPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <IonPage>
+    <IonContent class="ion-padding">
+      <IonSegment v-model="activeSegment">
+        <IonSegmentButton value="page-a">Page A</IonSegmentButton>
+        <IonSegmentButton value="page-b">Page B</IonSegmentButton>
+        <IonSegmentButton value="page-c">Page C</IonSegmentButton>
+      </IonSegment>
+
+      <PageA v-if="activeSegment === 'page-a'" />
+      <PageB v-else-if="activeSegment === 'page-b'" />
+      <PageC v-else />
+    </IonContent>
+  </IonPage>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { IonPage, IonContent, IonSegment, IonSegmentButton } from "@ionic/vue";
+import PageA from "@/views/UserPage/PageA.vue";
+import PageB from "@/views/UserPage/PageB.vue";
+import PageC from "@/views/UserPage/PageC.vue";
+
+const activeSegment = ref("page-a");
+</script>
